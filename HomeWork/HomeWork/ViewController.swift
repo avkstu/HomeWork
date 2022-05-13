00c5abc5153c2ff905930acb2ed5415d988ed1f6
avkstu <ntnkzmn@gmail.com>
2022-05-13 10:30:45 +0300

    homework #1

--- HomeWork/HomeWork/ViewController.swift
+++ HomeWork/HomeWork/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var a = 2.5
+    var b = 9.7
+    var c = 6.9
+    var d = 8.2
+    
+    var j = 7
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //  Целая часть
+        
+        let funcsum = Int (a) + Int (b) + Int (c) + Int (d)
+        
+        print(funcsum)
+        
+        //        Дробная часть
+        
+        let funcdotsum = (a - Double( Int (a))) + (b - Double( Int (b))) + (c - Double( Int (c))) + (d - Double( Int (d)))
+        
+        print(funcdotsum)
+        
+        //  Четное/нечетное
+        
+        if j % 2 == 0 {
+            print ("\(j) - четное") }
+        else {
+            print ("\(j) - нечетное")
+        }
+        
+        
     }
-
-
 }
-
